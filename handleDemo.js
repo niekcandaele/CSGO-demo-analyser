0d172af1d77edbcefaae13f9b015da3d054a54cb
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2020-01-10 14:24:23 +0100

    Handle player deaths

--- handleDemo.js
+++ handleDemo.js
@@ -1,65 +1,103 @@
 const demofile = require("demofile");
 const fs = require("fs");
 
-module.exports = (demoFile) => {
-    return new Promise((resolve, reject) => {
+module.exports = demoFile => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(demoFile, (err, buffer) => {
+      const demoFile = new demofile.DemoFile();
+      const data = {};
 
-        fs.readFile(demoFile, (err, buffer) => {
-            const demoFile = new demofile.DemoFile();
-            const data = {};
+      // Gets rewritten for every round
+      let roundData = generateNewRoundData();
 
-            demoFile.on("start", () => {
-                data.map = demoFile.header.mapName;
-                data.totalTime = demoFile.header.playbackTime;
-                data.players = {};
-            });
+      demoFile.on("start", () => {
+        data.map = demoFile.header.mapName;
+        data.totalTime = demoFile.header.playbackTime;
+        data.players = {};
+        data.rounds = [];
+        console.log(demoFile.gameEvents.gameEventList);
+      });
 
-            demoFile.gameEvents.on('round_start', () => {
-                for (const player of demoFile.players) {
-                    data.players[player.steam64Id] = {}
-                }
-            });
+      demoFile.gameEvents.on("round_start", () => {
+        for (const player of demoFile.players) {
+          if (!data.players[player.steam64Id]) {
+            data.players[player.steam64Id] = generateEmptyPlayerData(
+              player.steam64Id
+            );
+          }
+        }
+      });
 
-            /**
-             * v ALU CODE GOES HERE v
-             */
+      demoFile.gameEvents.on("player_death", e => {
+        const victim = demoFile.entities.getByUserId(e.userid);
+        const victimName = victim ? victim.name : "unnamed";
 
+        // Attacker may have disconnected so be aware.
+        // e.g. attacker could have thrown a grenade, disconnected, then that grenade killed another player.
+        const attacker = demoFile.entities.getByUserId(e.attacker);
+        const attackerName = attacker ? attacker.name : "unnamed";
 
-            // EXAMPLE: Kills
-            demoFile.gameEvents.on("player_death", e => {
-                const victim = demoFile.entities.getByUserId(e.userid);
-                const victimName = victim ? victim.name : "unnamed";
+        const headshotText = e.headshot ? " HS" : "";
 
-                // Attacker may have disconnected so be aware.
-                // e.g. attacker could have thrown a grenade, disconnected, then that grenade killed another player.
-                const attacker = demoFile.entities.getByUserId(e.attacker);
-                const attackerName = attacker ? attacker.name : "unnamed";
-
-                const headshotText = e.headshot ? " HS" : "";
-
-                console.log(`${attackerName} [${e.weapon}${headshotText}] ${victimName}`);
-            });
-
-            // EXAMPLE: Round end info
-
-            demoFile.gameEvents.on("round_officially_ended", e => {
-                const teams = demoFile.teams;
-
-                const terrorists = teams[2];
-                const cts = teams[3];
-
-                console.log(`\tTerrorists score ${terrorists.score}\n\tCTs: score ${cts.score}`)
-            });
-
-            /**
-             * ^ ALU CODE GOES HERE ^
-             */
-
-            // When demo file is finished, resolve the function call
-            demoFile.on("end", e => {
-                resolve(data)
-            });
-            demoFile.parse(buffer);
+        /*         console.log(
+          `${attackerName} [${e.weapon}${headshotText}] ${victimName}`
+        ); */
+        roundData.kills.push({
+          attacker: {
+            steamId: attacker.steam64Id,
+            equipmentValue: attacker.currentEquipmentValue,
+            cashSpendThisRound: attacker.cashSpendThisRound,
+            name: attacker.name,
+            hasC4: attacker.hasC4,
+            health: attacker.health,
+            isScoped: attacker.isScoped,
+            weapon: attacker.weapon.itemName,
+            freezeTimeEndEquipmentValue: attacker.freezeTimeEndEquipmentValue
+          },
+          victim: {
+            steamId: victim.steam64Id,
+            equipmentValue: victim.currentEquipmentValue,
+            cashSpendThisRound: victim.cashSpendThisRound,
+            name: victim.name,
+            hasC4: victim.hasC4,
+            health: victim.health,
+            isScoped: victim.isScoped,
+            freezeTimeEndEquipmentValue: victim.freezeTimeEndEquipmentValue
+          },
+          hs: e.headshot
         });
-    })
+      });
+
+      // EXAMPLE: Round end info
+
+      demoFile.gameEvents.on("round_officially_ended", e => {
+        const teams = demoFile.teams;
+
+        const terrorists = teams[2];
+        const cts = teams[3];
+
+        data.rounds.push(roundData);
+        roundData = generateNewRoundData();
+      });
+
+      // When demo file is finished, resolve the function call
+      demoFile.on("end", e => {
+        resolve(data);
+      });
+      demoFile.parse(buffer);
+    });
+  });
+};
+
+function generateNewRoundData() {
+  return {
+    kills: []
+  };
+}
+
+function generateEmptyPlayerData(steamId) {
+  return {
+    steamId: steamId,
+    chatMessages: []
+  };
 }
--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const fs = require("fs");
-const path = require('path');
-const handleDemo = require('./handleDemo');
+const path = require("path");
+const handleDemo = require("./handleDemo");
+const util = require("util");
 
-const demoDir = './demos'
+const demoDir = "./demos";
 
 fs.readdir(demoDir, async (err, files) => {
-    files = files.map(join(demoDir));
+  files = files.map(join(demoDir));
 
-    for (const file of files) {
-        if (file.endsWith('.dem')) {
-            console.log('------------------------------------\n')
-            console.log(`Handling demo file ${file}`)
-            console.log('------------------------------------\n')
-            let demoData = await handleDemo(file);
-            console.log('------------------------------------\n')
-            console.log(`Finished reading data from demo`)
-            console.log(demoData);
-            console.log('------------------------------------\n')
-        }
+  for (const file of files) {
+    if (file.endsWith(".dem")) {
+      console.log("------------------------------------\n");
+      console.log(`Handling demo file ${file}`);
+      console.log("------------------------------------\n");
+      let demoData = await handleDemo(file);
+      console.log("------------------------------------\n");
+      console.log(`Finished reading data from demo`);
+      console.log(util.inspect(demoData, { depth: null, colors: true }));
+      console.log("------------------------------------\n");
     }
-
+  }
 });
 
 function join(dir) {
-    return function (file) {
-        return path.join(dir, file);
-    };
+  return function(file) {
+    return path.join(dir, file);
+  };
 }
